9cb84b90903b046566260f4e8da0677d21612285
vorden <voronkodenis@mail.ru>
2024-04-09 00:34:27 +0300

    version 3.1

--- #game_core_v3.py
+++ #game_core_v3.py
@@ -1,4 +1,5 @@
 #game_core_v3
+from numpy import random
 def game_core_v3(number: int = 1) -> int:
     """
     Args:
@@ -8,7 +9,22 @@ def game_core_v3(number: int = 1) -> int:
         int: Число попыток
     """
     # Ваш код начинается здесь
+    count = 0
+    tmp_number = number
+    predict_number = random.randint(1, 101)
+    while True:
+        count += 1
+        if number == predict_number: break    
+        if predict_number < 16: predict_number -=1
+        if 15 < predict_number < 31: predict_number -= 15
+        if 30 < predict_number < 46: predict_number -= 30
+        if 45 < predict_number < 61: predict_number -= 45
+        if 60 < predict_number < 76: predict_number -= 60
+        if 75 < predict_number < 101: predict_number -= 75
 
+            
     # Ваш код заканчивается здесь
 
     return count
+print(f'Run benchmarking for game_core_v3: {game_core_v3()}')
+
