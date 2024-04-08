a9ed3520cba2995bb69236566cb855bb240d0fd4
vorden <voronkodenis@mail.ru>
2024-04-08 23:06:30 +0300

    game_core_v3.0

---
+++ #game_core_v3.py
@@ -0,0 +1,14 @@
+#game_core_v3
+def game_core_v3(number: int = 1) -> int:
+    """
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    # Ваш код начинается здесь
+
+    # Ваш код заканчивается здесь
+
+    return count
