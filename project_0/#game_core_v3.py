7143b64951a6f4fea5f3423138a23d50841e52dc
vorden <voronkodenis@mail.ru>
2024-04-10 17:15:30 +0300

    final version

--- project_0/#game_core_v3.py
+++ project_0/#game_core_v3.py
@@ -1,6 +1,7 @@
 from numpy import random
 from numpy import mean
 
+
 def game_core_v3(number: int = 1) -> int:
     """
     Args:
@@ -14,21 +15,22 @@ def game_core_v3(number: int = 1) -> int:
     max_lim = 101  # верхняя граница
     # Запускаем бесконечный цикл поиска загаданного числа
     while True:
+        
         # Вычисляем предполагаемое число и сравниваем с загаданным
         count += 1                                   # увеличиваем на 1 число попыток
         predict_number = (max_lim + min_lim) // 2  # предполагаемое число
         result = number - predict_number             # результат сравнения 
-
+        
         # Выходим из цикла, если число угадано
         if result == 0: break
         # Изменяем границы интервала поиска, если число не угадано
+        
         if result < 0:                                # если результат сравнения отрицательный
             max_lim = predict_number                # то правую границу смещаем в предполагаемое число
         else:                                         # если результат сравнения положительный
             min_lim = predict_number                # левую границу смещаем в предполагаемое число
     return count
 
-    return count
 
 def score_game(random_predict) -> int:
     """За какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
@@ -40,7 +42,7 @@ def score_game(random_predict) -> int:
         int: среднее количество попыток
     """
     count_ls = []
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random.seed(1)  # фиксируем сид для воспроизводимости
     random_array = random.randint(1, 101, size=(10000))  # загадали список чисел
 
     for number in random_array:
@@ -48,6 +50,8 @@ def score_game(random_predict) -> int:
 
     score = int(mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
+
+
 
 print('Run benchmarking for game_core_v3: ', end='')
 score_game(game_core_v3)
