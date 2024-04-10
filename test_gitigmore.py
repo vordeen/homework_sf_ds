5f77c1b783a77b4227a69f7af43c1a46c2ec49c5
vorden <voronkodenis@mail.ru>
2024-04-10 14:20:38 +0300

    version 3.3

--- test_gitigmore.py
+++ test_gitigmore.py
@@ -1,4 +1,3 @@
-#game_core_v3
 from numpy import random
 def game_core_v3(number: int = 1) -> int:
     """
@@ -10,21 +9,13 @@ def game_core_v3(number: int = 1) -> int:
     """
     # Ваш код начинается здесь
     count = 0
-    tmp_number = number
     predict_number = random.randint(1, 101)
-    while True:
+    while number != predict_number:
         count += 1
-        #if number == predict_number: break
-        if predict_number > number:
+        if number > predict_number:
+            predict_number += 2
+        elif  number < predict_number:
             predict_number //= 2
-        elif  predict_number < number:
-            predict_number //= 2
-        else:
-              break
-
-            
-    # Ваш код заканчивается здесь
 
     return count
-print(f'Run benchmarking for game_core_v3: {game_core_v3()}')
-
+    
